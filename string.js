89f88d9181c14d80204611426c93c7a0ed61187d
Paman Bob <60291644+pamanbobb@users.noreply.github.com>
2021-06-20 20:23:18 +0700

    Create string.js

---
+++ string.js
@@ -0,0 +1,6 @@
+var string = new Object();
+var get_one = ['rate my arch 1-10', 'Homegrown Hotwife', 'Craving to be milked Glass of milkSplashing sweat symbol', 'Catch the full head video and the Cum shot in my message or the link below Down pointing backhand indexSmiling face with horns', 'Wanna give me another load? check it here', 'RT if you'd fuck a MILF, check my full video for free', 'I swear he love eating me, do you ? available contact me here', 'RT if you love a filthy slut ! check me here bae'];
+var siji =  Math.floor(Math.random() * get_one.length);
+string.docking = get_one[siji];
+var out = JSON.stringify(string);
+console.log(JSON.parse(out));
