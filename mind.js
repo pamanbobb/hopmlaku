f7e0865c8e98dc6d35fb1b78e5a4897a97cc6772
Paman Bob <60291644+pamanbobb@users.noreply.github.com>
2021-06-20 20:51:25 +0700

    Delete mind.js

--- mind.js
+++
@@ -1,3 +0,0 @@
-var get_one = ['rate my arch 1-10', 'Homegrown Hotwife', 'Craving to be milked Glass of milkSplashing sweat symbol', 'Catch the full head video and the Cum shot in my message or the link below Down pointing backhand indexSmiling face with horns', 'Wanna give me another load? check it here', 'RT if you'd fuck a MILF, check my full video for free', 'I swear he love eating me, do you ? available contact me here', 'RT if you love a filthy slut ! check me here bae'], siji =  Math.floor(Math.random() * get_one.length);
-const op = {kata : get_one[siji]};
-JSON.stringify(op);
